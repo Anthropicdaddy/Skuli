3e688049abd3cc6ce151852f2f36446eaf0cea5b
Anthropicdaddy <petervfk1@gmail.com>
2026-07-24 06:08:50 +0000

    flatten: move apps to root level (staff/, student-parent/) for simpler Vercel deploy

--- packages/shared/src/index.ts
+++
@@ -1,6 +0,0 @@
-export { prisma } from "./lib/prisma";
-export { cn } from "./lib/utils";
-export { hashPassword, comparePassword, signStudentToken, signParentToken, verifyToken, getTokenFromRequest } from "./lib/auth";
-export { sendStaffInviteEmail, sendWelcomeEmail, sendParentWelcomeEmail } from "./lib/email";
-export { lowerPrimarySubjects, upperPrimarySubjects, gradeNames, getSubjectsForGrade } from "./lib/cbc";
-export type { StudentSession, ParentSession } from "./lib/auth";
